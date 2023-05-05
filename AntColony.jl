7e80c28cd94fec38fee60c297424416f158521d4
Arthur Ferraz <arthursnc@gmail.com>
2023-05-05 12:35:11 -0300

    removendo backup de AntColony

--- AntColony.jl
+++ AntColony.jl
@@ -1,96 +1,100 @@
 include("tsp.jl")
 
-function antColony!(tsp, sol, N=500, K=100)
+function antColony(tsp, N=500, K=100)
     ro=0.1
     alfa = 1
     beta = 5
-	feromonios = zeros(Int64, tsp.dimension, tsp.dimension)
+    limite = 10
+    contad = limite
+    feromonios = zeros(Float64, tsp.dimension, tsp.dimension)
+
     #definindo valor inicial para feromonios
-    custo = 0
+    soma = 0.0
     for i = 1:tsp.dimension-1
         for j = i+1:tsp.dimension
             soma+=tsp.weights[i, j]
         end
     end
-    foromonios[:] .= tsp.dimension/soma
-
-	caminhos = zeros(Int64, N, tsp.dimension)
+    feromonios .= tsp.dimension/soma
+    caminhos = zeros(Int64, N, tsp.dimension)
     naoVisitadas = ones(Bool, tsp.dimension)
-    probLocal = zeros(tsp.dimension)
-	for k = 1:K #para cada iteração
-		for n = 1:N # para cada formiga
-           cidade = 1
-           i = rand(1:tsp.dimension)#escolha uma cidade de onde partir
-           while !isempty(filter(n --> n == true, naoVisitadas))#para cada cidade nao visitada
-               naoVisitadas[i] = false
-               #calculando probabilidades de visitar cidades vizinhas
-               probLocal .= feromonios[i, :].^alf.*tsp.weights[i, :].^beta
-               #removendo probabilidade para cidades ja visitadas
-               probLocal[naoVisitadas .== false] .= 0
-               #criando soma acumulada
-               probLocal /= sum(probLocal)
-               probLocal = cumsum(probLocal)
-               #escolhendo caminho
-               j = rand()
-               j = findfirst(j .<= probLocal)
-               i = j
-               #registrando rota
-               caminhos[n, cidade] = i
-               cidade += 1
-           end
-#busca local aqui
-           naoVisitadas .= 1 
-		end
+    probLocal = zeros(Float64, tsp.dimension)
+    melhor = []
+
+    for k = 1:K #para cada iteração
+        for n = 1:N # para cada formiga
+            i = rand(1:tsp.dimension)#escolha uma cidade de onde partir
+            caminhos[n, 1] = i
+            naoVisitadas[i] = false
+
+            for cidade = 2:tsp.dimension
+                #calculando probabilidades de visitar cidades vizinhas
+                probLocal .= feromonios[i, :].^alfa.*tsp.weights[i, :].^-beta
+                #removendo probabilidade para cidades ja visitadas
+                probLocal[naoVisitadas .== false] .= 0
+
+                #criando soma acumulada
+                probLocal /= sum(probLocal)
+                probLocal = cumsum(probLocal[:])
+
+                #escolhendo caminho
+                j = rand()
+                i = findfirst(j .<= probLocal)
+                #registrando rota
+                naoVisitadas[i] = false
+                caminhos[n, cidade] = i
+            end
+            #busca local aqui
+            Opt2!(tsp, caminhos[n, :])
+            naoVisitadas .= true
+        end
 
         #atualizando feromonios
-        #evaporando feromonios
-        feromonios .= (1-ro).*feromonios
-        for 
-	end
+        feromonios[:] .= (1-ro).*feromonios[:]#evaporando feromonios
+        melhorLocal = 1
+        custoLocal = 0
+        #para cada formiga
+        for n = 1:N
+            custoLocal = tspdist(tsp, caminhos[n, :])
+            if n % 5 == 0
+                println("custoLocal=", custoLocal, " metros")
+            end
+            melhorLocal = (custoLocal < tspdist(tsp, caminhos[melhorLocal, :])) ? n : melhorLocal
+            #atualiza os feromonios da rota
+            for j = 1:tsp.dimension-1
+                feromonios[caminhos[n, j], caminhos[n, j+1]] += 1.0 / custoLocal
+                feromonios[caminhos[n, j+1], caminhos[n, j]] += 1.0 / custoLocal
+            end
+            feromonios[caminhos[n, tsp.dimension], caminhos[n, 1]] += 1.0 / custoLocal
+            feromonios[caminhos[n, 1], caminhos[n, tsp.dimension]] += 1.0 / custoLocal
+        end
+        #reforçando melhorLocal formiga
+        custoLocal = tspdist(tsp, caminhos[melhorLocal, :])#custo da melhor formiga
+
+        #atualiza os feromonios da rota
+        for j = 1:tsp.dimension-1
+            feromonios[caminhos[melhorLocal, j], caminhos[melhorLocal, j+1]] += 1.0 / custoLocal
+            feromonios[caminhos[melhorLocal, j+1], caminhos[melhorLocal, j]] += 1.0 / custoLocal
+        end
+        feromonios[caminhos[melhorLocal, tsp.dimension], caminhos[melhorLocal, 1]] += 1.0 / custoLocal
+        feromonios[caminhos[melhorLocal, 1], caminhos[melhorLocal, tsp.dimension]] += 1.0 / custoLocal
+
+        if isempty(melhor)
+            melhor = copy(caminhos[melhorLocal, :])
+        elseif custoLocal < tspdist(tsp, melhor)
+            melhor .= caminhos[melhorLocal, :]
+            contad = limite
+        end
+        #tspplot(tsp, caminhos[melhorLocal, :])
+        tspplot(tsp, melhor, "Formiga com menor trajetoria:")
+        #sleep(0.08)
+        println("Menor trajeto até então: ", tspdist(tsp, melhor), " metros")
+        if contad == 0
+            break
+        end
+        contad -= 1
+    end
+    println("Menor trajeto encontrado é ", tspdist(tsp, melhor) - tsp.optimal, " metros menor que o trajeto ótimo do problema.(", round((tspdist(tsp, melhor) - tsp.optimal)/tsp.optimal; digits = 3),"% de erro)")
+    return melhor
 end
 
-function simAnel!(tsp, sol, t0, alfa, maxit, maxitSub)
-	moves=[0.05, 0.8, 0.15]
-	n = tsp.dimension
-	T = t0
-	diff = 0
-	roleta = cumsum(moves)
-	dist = tspdist(tsp, sol)
-	solGlob=copy(sol)
-
-	#inicia loop temperatura reduzindo
-	for i = 1:maxit
-		#inicia loop temperatura constante
-		solLoc = zeros(Int64, length(sol))
-		for j = 1:maxit
-			solLoc .= sol#copia solucao atual
-			x = rand()
-			x = findfirst(x .<= roleta)
-			a = rand(1:length(sol)-1)
-			b = rand(a+1:length(sol))
-			if x == 1
-				swap!(solLoc, a, b)
-			elseif x == 2
-				reversao!(solLoc, a, b)
-			else
-				insercao!(solLoc, a, b)
-			end
-			diff = tspdist(tsp, solLoc) - tspdist(tsp, sol)
-			#if diff <= 0 || rand() <= exp(-1*(diff/T))
-			if rand() <= exp(-1*(diff/T))
-				sol .= solLoc
-			end
-		end
-		solGlob .= (tspdist(tsp, sol) <= tspdist(tsp, solGlob)) ? sol : solGlob
-		T *= alfa
-		i += 1
-
-		tspplot(tsp, sol)
-		sleep(0.08)
-		println(tspdist(tsp, sol))
-	end
-	sol .=solGlob
-	tspplot(tsp, sol)
-	sleep(0.08)
-	println(tspdist(tsp, sol))
-end
