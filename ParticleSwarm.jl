91786296de406856071bc8b56aced71fd84e8cad
ferrazarthur <arthursnc@gmail.com>
2023-05-19 14:23:47 -0300

    [feat] enxame de partículas finalizado

--- ParticleSwarm.jl
+++ ParticleSwarm.jl
@@ -1,63 +1,60 @@
 include("tsp.jl")
 include("funcoes-teste.jl")
 
-function partSwarm(f, l, u; dimension=100, nPopulation=50, kIter=20000, Cp=0.2, Cg=0.6, W=0.1)
-    #verificando requisitos
+function partSwarm(f, l, u; dimension=100, nPopulation=50, kIter=20000, Cp=2.05, Cg=2.05, W=0.7, Vmax = 1, VmaxCalibr=true)
+    if VmaxCalibr == true
+        Vmax = min(Vmax, (u-l)/sqrt(nPopulation))
+    end
     Kmax = floor(0.1*kIter)
-    #gerando população aleatoriamente
-    currentPop = []
+
+    #gerando população inicial
     pos = zeros(Float64, nPopulation, dimension)
     vel = zeros(Float64, nPopulation, dimension)
     best = zeros(Float64, nPopulation, dimension)
-    bestG = 0
+    bestG = 1
+    #inicializando os vetores
     for i = 1:nPopulation
-        pos[i] .= [rand(l:0.001:u, dimension)]
+        for j = 1:dimension
+            pos[i, j] = (u-l)*rand()+l
+            best[i, j] = pos[i, j]
+            vel[i, j] = 2*Vmax*rand()-Vmax
+        end
     end
-    #criando vetor utilizado para armazenar crossovers
-    crossOver = zeros(Float64,dimension)
-    minimo = f(currentPop[1])
-    #mutacao = Array{Float64, dimension}
-    mutacao = zeros(Float64, dimension)
+
     Kparada = Kmax
     for k = 1:kIter
         for i = 1:nPopulation
-            #criando indivíduo mutado
-            #escolhendo três indivíduos distintos entre si e entre o indivíduo i
-            escolhas = [i]#os escolhidos serão os índices 2, 3 e 4
-            for j = 1:4
-                append!(escolhas, rand((1:nPopulation)[1:nPopulation .∉ [escolhas]]))  
+            #atualiza melhores individuos de cada posição
+            if f(pos[i, :]) <= f(best[i, :]) 
+                best[i, :] .= pos[i, :]
             end
-            #garantindo que a mutação não saia do domínio do problema
-            for j = 1:dimension
-                mutacao[j] = max(l, min(u, currentPop[escolhas[2]][j] + ((Fmax-Fmin)*rand() + Fmin)*currentPop[escolhas[3]][j]-currentPop[escolhas[4]][j]))
-            end
-
-            #realizando cross-over entre i e o mutado
-            #escolha de índice aleatório para garantir crossOver != currentPop
-            randInd = rand(1:dimension)
-            randVal = 0
-            for j = 1:dimension
-                randVal = rand()
-                crossOver[j] = (randVal <= CR || randInd == j) ? mutacao[j] : currentPop[i][j]
-            end
-
-            #se o crossOver tiver melhor performance, substituirá o individuo atual.
-            if f(crossOver[:]) <= f(currentPop[i][:])
-                currentPop[i][:] .= crossOver[:]
+            #atualiza melhor global
+            if f(pos[i, :]) <= f(best[bestG, :])
                 Kparada = Kmax
+                bestG = i
+            end
+            #gera valores aleatórios para peso do melhor global e melhor local no calculo de v
+            Rg = rand(dimension)
+            Rp = rand(dimension)
+            for j = 1:dimension
+                vel[i, j] = W*vel[i, j] + Cp*Rp[j]*(best[i, j] - pos[i, j]) + Cg*Rg[j]*(best[bestG, j] - pos[i, j])
+                #garante que velocidade não fuja dos limites estabelecidos
+                vel[i, j] = max(-Vmax, min(Vmax, vel[i, j]))
+            end
+            #andando com a população
+            pos[i, :] .= pos[i, :] + vel[i, :]
+            #garantindo que as posições dos pontos nao fujam dos limites
+            for j = 1:dimension
+                pos[i, j] = max(l, min(u, pos[i, j]))
             end
         end
-        for i = 1:nPopulation
-            loc = f(currentPop[i][:])
-            minimo = loc <= minimo ? loc : minimo
-        end
-        println("Iteração: ", k, " Mínimo: ", minimo)
+        println("Iteração: ", k, " Mínimo: ", f(best[bestG, :]))
         Kparada -= 1
         if Kparada == 0
             break
         end
     end
 
-    return currentPop
+    return pos[bestG, :]
 end
 
